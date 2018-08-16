bb9d056738b80905fc16f6c1af036cf8292299a9
Modog <gabstiy@gmail.com>
2018-08-16 06:58:24 -0300

    Update map.lua

--- map.lua
+++ map.lua
@@ -1,10 +1,10 @@
 -- map begin
-function map_collider()
-	if player.x < 0 then
-		player.x = 0
+function map_collider(obj)
+	if obj.x < 0 then
+		obj.x = 0
 	end
-	if player.x + player.image:getWidth() > 800 then
-		player.x = 800 - player.image:getWidth()
+	if obj.x + obj.image:getWidth() > 800 then
+		obj.x = 800 - obj.image:getWidth()
 	end
 end
 -- map end
